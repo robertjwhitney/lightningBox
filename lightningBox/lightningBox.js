a04727b627ef62aa5cb4580b19820e05ea75efb3
robertjwhitney <robertj.whitney@gmail.com>
2010-02-05 02:27:05 -0500

    refactoring js to use jquery.data

--- lightningBox/lightningBox.js
+++ lightningBox/lightningBox.js
@@ -1,30 +1,37 @@
-$(document).ready(function() {
-  //hide the box no matter what the current hash is
+$(function(){
+  $("body").append("<ul id='lightning'></ul>");
   $("#lightning").hide();
-  var bolts = $("a[rel=lightning]");
-  bolts.click( function() {
-    $("#lightning").fadeIn("fast");
-    //stash current scroll position
-    $("#close").html(f_scrollTop());
+  var bolts = $("a[rel='lightning']");
+  var boltCount = bolts.length;
+  var lightning = $("#lightning");
+  var bolt = [];
+  var jLink = [];
+  var scrollPos = "";
+  bolts.each( function() {
+    bolt.push(this)
   });
-  
-  //get some close and nav links going
-  var closeLink = "<a href='javascript:void(0);' class='lightning-close'><div class='close-lightning-box'>x</div></a>";
-  $("#lightning .box").each( function() {
-      var boxParent = $(this).parent();
-      var nextHref = boxParent.next().attr("id");
-      var prevHref = boxParent.prev().attr("id");
-      var navLinks = "<div id='lightning-nav'><a href=#" + prevHref + " class='prev'>&#8592; Prev</a> <a href='#" + nextHref + "' class='next'>Next &#8594;</a></div>";
-      $(this).prepend(closeLink).append(navLinks);
+  for (var j = 0; j < boltCount; j++) {
+    jLink[j] = AddSlide( bolt[j], j, "alt");
+  };
+  var i = 0;
+  //Make links open lightbox instead
+  bolts.each( function() { 
+    $(this).attr("href", "#slide" + i);
+    i++;
   });
-  
-  //disable first and last slide nav
-  $("#lightning .first #lightning-nav .prev").addClass("lightning-nav-disabled").attr('href', 'javascript:void(0);');
-  $("#lightning .last #lightning-nav .next").addClass("lightning-nav-disabled").attr('href' , 'javascript:void(0);');
-  
-  //create some lightning transition
+  bolts.click( function(){
+    lightning.show();
+    scrollPos = f_scrollTop();
+  });  
+  $(".close-lightning-box").click( function() {
+    lightning.hide();
+    window.scroll(0, scrollPos);
+  });  
+  //disable first and last links
+  $("#lightning li:first-child #lightning-nav #prev").addClass("lightning-nav-disabled").attr('href', 'javascript:void(0);');
+  $("#lightning li:last-child #lightning-nav #next").addClass("lightning-nav-disabled").attr('href' , 'javascript:void(0);');
+  //add some animation
   $("#lightning-nav a").click( function(){
-    var lightning = $("#lightning");
     lightning.animate({
         opacity: 0.5,
       }, 80, function() {
@@ -33,28 +40,37 @@ $(document).ready(function() {
           }, 250);
     });
   });
-  
-  //close the box
-  $(".lightning-close").click( function(){
-    $("#lightning").fadeOut("fast");
-    //grab previous scroll position
-    var returnScroll = $("#lightning-close").text();
-    //set scroll position back
-    window.scroll(0,returnScroll);
-  });
-  
-  //do some scroll cross-browser voodoo
-  function f_scrollTop() {
-    return f_filterResults (
-      window.pageYOffset ? window.pageYOffset : 0,
-      document.documentElement ? document.documentElement.scrollTop : 0,
-      document.body ? document.body.scrollTop : 0
-    );
-  }
-  function f_filterResults(n_win, n_docel, n_body) {
-    var n_result = n_win ? n_win : 0;
-    if (n_docel && (!n_result || (n_result > n_docel)))
-      n_result = n_docel;
-    return n_body && (!n_result || (n_result > n_body)) ? n_body : n_result;
-  }
 });
+// Add Slide to DOM.
+function AddSlide( imgHref, intID, alt) {
+  var jParent = $( "ul#lightning" );
+  var jListItem = $( "<li id='slide" + intID + "'></li>" );
+  var jbox = $( "<div class='box'><img src='" + imgHref + "' /></div>");
+  var jLink = $( "<img src='" + imgHref + "' />" );
+  var jclose = $( "<div class='close-lightning-box'>x</div>");
+  var jnav = $("<div id='lightning-nav'><a href='#slide" + (intID - 1) + "' alt='previous' id='prev'>&#8592; Prev</a><a href='#slide" + (intID + 1) + "' alt='next' id='next'>Next &#8594;</a></div>");
+  // Associate data with this slide.
+  jLink.data( "Data", {
+    Href: imgHref,
+    ID: intID,
+    alt: alt
+  }); 
+  // Add the slides to the list item and add the list item to the DOM.
+  jParent.append( jListItem.append( jbox.prepend(jclose).append(jnav) ) );
+  // Return new slide.
+  return( jLink );
+}
+//do some cross-browser scroll position magic
+function f_scrollTop() {
+  return f_filterResults (
+    window.pageYOffset ? window.pageYOffset : 0,
+    document.documentElement ? document.documentElement.scrollTop : 0,
+    document.body ? document.body.scrollTop : 0
+  );
+}
+function f_filterResults(n_win, n_docel, n_body) {
+  var n_result = n_win ? n_win : 0;
+  if (n_docel && (!n_result || (n_result > n_docel)))
+    n_result = n_docel;
+  return n_body && (!n_result || (n_result > n_body)) ? n_body : n_result;
+}
